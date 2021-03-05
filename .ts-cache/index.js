c47f7074cefff4663f0d4ee9492e681231b807b3
minami0601 <minami.love.bb@icloud.com>
2021-03-05 22:48:47 +0900

    TypeScript で型を検査

--- .ts-cache/index.js
+++ .ts-cache/index.js
@@ -1,2 +1,3 @@
-const message = document.querySelector("h1").textContent;
+var _a;
+const message = (_a = document.querySelector("h1")) === null || _a === void 0 ? void 0 : _a.textContent;
 console.log(message);

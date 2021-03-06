72b0cb321ef93b93165130e1f513141a5cafee0b
minami0601 <minami.love.bb@icloud.com>
2021-03-06 21:36:47 +0900

    styled-components で DOM の実体を抽象化

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>
-      <header>
-        <div>Kanban board</div>
+    <Container>
+       <Header>
+         <Logo>Kanban board</Logo>
+ 
+         <CardFilter placeholder="Filter cards" />
+       </Header>
 
-        <input placeholder="Filter cards" />
-      </header>
+       <MainArea>
+       <Column>
+           <ColumnHeader>TODO</ColumnHeader>
+ 
+           <Card>朝食をとる🍞</Card>
+           <Card>SNSをチェックする🐦</Card>
+           <Card>布団に入る (:3[___]</Card>
+         </Column>
 
-      <div>
-        <section>
-          <h3>TODO</h3>
+         <Column>
+           <ColumnHeader>Doing</ColumnHeader>
+ 
+           <Card>顔を洗う👐</Card>
+           <Card>歯を磨く🦷</Card>
+         </Column>
+         <Column>
+           <ColumnHeader>Waiting</ColumnHeader>
+         </Column>
 
-          <article>朝食をとる🍞</article>
-          <article>SNSをチェックする🐦</article>
-          <article>布団に入る (:3[___]</article>
-        </section>
-
-        <section>
-          <h3>Doing</h3>
-
-          <article>顔を洗う👐</article>
-          <article>歯を磨く🦷</article>
-        </section>
-
-        <section>
-          <h3>Waiting</h3>
-        </section>
-
-        <section>
-          <h3>Done</h3>
-
-          <article>布団から出る (:3っ)っ -=三[＿＿]</article>
-        </section>
-      </div>
-    </div>
+         <Column>
+           <ColumnHeader>Done</ColumnHeader>
+ 
+           <Card>布団から出る (:3っ)っ -=三[＿＿]</Card>
+         </Column>
+         </MainArea>
+    </Container>
   )
 }
+const Container = styled.div``
+ 
+const Header = styled.div``
+
+const Logo = styled.div``
+
+const CardFilter = styled.input``
+
+const MainArea = styled.div``
+
+const Column = styled.div``
+
+const ColumnHeader = styled.div``
+
+const Card = styled.div``
+
 
 ReactDOM.render(<App />, document.getElementById('app'))
